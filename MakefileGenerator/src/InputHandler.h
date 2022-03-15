c3ec23e8aec4dd90e7243643436587d4315a21fd
pyvyx <pyvyx@web.de>
2022-03-15 23:27:45 +0100

    Moved the input handling function into the InputHandler header file

--- MakefileGenerator/src/InputHandler.h
+++ MakefileGenerator/src/InputHandler.h
@@ -184,3 +184,56 @@ public:
 		std::cout << "]\n";
 	}
 };
+
+
+static void PrintHelpMessage(const char* fileName) {
+
+	std::cout << "Usage: " << fileName << " <options>\n";
+	std::cout << "       -g or -gui = start in GUI mode\n";
+	std::cout << "       <filename> = load this config file\n";
+	std::cout << "If you're providing a file name without using the GUI mode\n";
+	std::cout << "the makefile will immediately be created\n";
+}
+
+
+struct Input {
+	bool guiMode;
+	std::string filePath;
+	bool error;
+};
+
+
+Input HandleInput(int argc, char** argv) {
+
+	bool usingGuiMode = true;
+	std::string filePath;
+	bool error = false;
+
+	InputHandler ip(argc, argv);
+	ip.lowerAll();
+	if (ip.contains("-h")) {
+		PrintHelpMessage(argv[0]);
+		error = true;
+	}
+	else if (ip.size() > 1)
+	{
+		if (!ip.contains("-g") && !ip.contains("-gui"))
+			usingGuiMode = false;
+		if (ip.size() > 2) {
+			int location = ip.location("-g");
+			if (location == -1)
+				location = ip.location("-gui");
+			if (location == 1) {
+				filePath = ip[2];
+			}
+			else {
+				filePath = ip[1];
+			}
+		}
+		else if (!usingGuiMode) {
+			filePath = ip[1];
+		}
+	}
+
+	return { usingGuiMode, filePath, error };
+}
--- MakefileGenerator/src/main.cpp
+++ MakefileGenerator/src/main.cpp
@@ -1,47 +1,14 @@
-#include <filesystem>
-
 #include "Application.h"
 #include "InputHandler.h"
 
-void PrintHelpMessage(const char* fileName) {
-
-	std::cout << "Usage: " << fileName << " <options>\n";
-	std::cout << "       -g or -gui = start in GUI mode\n";
-	std::cout << "       <filename> = load this config file\n";
-	std::cout << "If you're providing a file name without using the GUI mode\n";
-	std::cout << "the makefile will immediately be created\n";
-}
 
 int main(int argc, char** argv)
 {
-	bool usingGuiMode = true;
-	std::string filePath;
+	Input ip = HandleInput(argc, argv);
+	if (ip.error)
+		return 1;
 
-	InputHandler ip(argc, argv);
-	ip.lowerAll();
-	if (ip.contains("-h")) {
-		PrintHelpMessage(argv[0]);
-		return 0;
-	}
-	if (ip.size() > 1)
-	{
-		if(!ip.contains("-g") && !ip.contains("-gui"))
-			usingGuiMode = false;
-		if (ip.size() > 2) {
-			int location = ip.location("-g");
-			if (location == -1)
-				location = ip.location("-gui");
-			if (location == 1) {
-				filePath = ip[2];
-			} else {
-				filePath = ip[1];
-			}
-		} else if (!usingGuiMode) {
-			filePath = ip[1];
-		}
-	}
-
-	if (!App::StartApplication(usingGuiMode, filePath))
+	if (!App::StartApplication(ip.guiMode, ip.filePath))
 		return 1;
 	return 0;
 }
